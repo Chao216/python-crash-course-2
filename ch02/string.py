a3c3dffd33df8a26624f8d59aed5c9e0c6b75ace
Chao Jiang <co.jiaan@proton.me>
2022-11-24 21:34:02 +0800

    auto push 2022/11/24@21:34:02

---
+++ ch02/string.py
@@ -0,0 +1,12 @@
+name = "chao jiang"
+print(name.title())
+print(name.lower())
+print(name.upper())
+
+first = "Lei"
+last = "Yu"
+full = f"{last} {first}"
+print(full.title())
+print(full.upper())
+print(full.lower())
+
