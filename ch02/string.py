6a3cba3f868ebcf5d12661864f834152614226ee
Chao Jiang <co.jiaan@proton.me>
2022-11-24 21:47:33 +0800

    auto push 2022/11/24@21:47:33

--- ch02/string.py
+++ ch02/string.py
@@ -10,3 +10,7 @@ print(full.title())
 print(full.upper())
 print(full.lower())
 
+# 制表符与换行符
+# 可以使用\n 换行print \t 对齐
+
+print("\n\tCountries: \n\tRussia \n\tFinland \n\tHungary \n\tSlovakia \n\tCzech \n\tGermany \n\tEstonia \n\tItaly \n\tGreek \n\tAustria \n\tLativa \n\tLithunia \n\tPoland")
