a445265f8f06c7adcdb65a10e76981a04ac90a6b
Chao Jiang <co.jiaan@proton.me>
2022-11-25 22:40:01 +0800

    auto push 2022/11/25@22:40:01

--- ch06/nesting.py
+++ ch06/nesting.py
@@ -1,3 +1,4 @@
+# list in dictionary
 dictionary = {
         'first':[1],
         'second':[2],
@@ -8,3 +9,18 @@ for key, value in dictionary.items():
     print(key)
     for el in value:
         print(f"\t{el}")
+
+# dictionary in dictionary
+
+drink_order = {
+        'chao':{'drink':'espresso', 'unit':2, 'serving':'hot'},
+        'vova':{'drink':'green tea', 'unit':1, 'serving':'hot'},
+        'apurva':{'drink':'masala chai', 'unit':1, 'serving':'hot'}
+        }
+
+for people, drinks in drink_order.items():
+    print(f"\n{people}")
+    print(f"\t{drinks['drink']}")
+    print(f"\t{drinks['unit']}")
+    print(f"\t{drinks['serving']}")
+    print('\n let us use a smart way to print !')
