c6b25fa8a21e45bdf703bad8681abfb03772a677
Chao Jiang <co.jiaan@proton.me>
2022-11-30 18:39:17 +0800

    auto push 2022/11/30@18:39:17

---
+++ ch07/input.py
@@ -0,0 +1,12 @@
+message = input("What is your name? ")
+
+print(f"\n hello {message}, how are you? ")
+
+# input() 得到的是字符串， 可以使用int()将字符串转换为整数
+
+number = input("what is your favorite number? ")
+
+my_num = int(number) + 6
+
+print(f"\nmy favorite number is {my_num}")
+
