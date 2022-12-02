634ff94d2201ce78db8943e75e88af9dcb9269d1
Chao Jiang <co.jiaan@proton.me>
2022-12-02 17:58:22 +0800

    auto push 2022/12/02@17:58:22

---
+++ ch11/language_survey.py
@@ -0,0 +1,7 @@
+from survey import AnonymousSurvey
+
+question = "what languages do you speak?"
+
+my_survey = AnonymousSurvey(question)
+
+my_survey.show_question()
---
+++ ch11/survey.py
@@ -0,0 +1,16 @@
+class AnonymousSurvey:
+    def __init__(self, question):
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        print(self.question)
+
+    def store_response(self, new_response):
+        self.responses.append(new_respons)
+
+    def show_responses(self):
+        print("here are the results:")
+
+        for res in self.responses:
+            print(f"\t---{res}")
