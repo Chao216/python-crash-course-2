29eba72529ccd64f8fd8909b64c74ba13796ed0f
Chao Jiang <co.jiaan@proton.me>
2022-12-04 16:29:42 +0800

    auto push 2022/12/04@16:29:42

--- Projects/Alien_game/alien_invasion.py
+++ Projects/Alien_game/alien_invasion.py
@@ -1,6 +1,13 @@
 import sys
 import pygame
 
-class Alien_Invasion:
-    def __init__(self):
+class Alien_Invasion:# a class to manage game resource and behaviors
+    def __init__(self):# init the game
         pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien_Invasion")
+
+    def run_game(self):
+        while True:
+
