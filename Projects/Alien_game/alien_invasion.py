7bafc0183c293cce9fa213f8794ac21abff374f6
Chao Jiang <co.jiaan@proton.me>
2022-12-04 16:35:31 +0800

    auto push 2022/12/04@16:35:31

--- Projects/Alien_game/alien_invasion.py
+++ Projects/Alien_game/alien_invasion.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 
-class Alien_Invasion:# a class to manage game resource and behaviors
+class AlienInvasion:# a class to manage game resource and behaviors
     def __init__(self):# init the game
         pygame.init()
 
@@ -10,4 +10,13 @@ class Alien_Invasion:# a class to manage game resource and behaviors
 
     def run_game(self):
         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
