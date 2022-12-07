4a083d4844cc85c947e7ce5ebc4af7bc895f6cf8
Chao Jiang <co.jiaan@proton.me>
2022-12-07 19:54:51 +0800

    auto push 2022/12/07@19:54:51

---
+++ python_automation/ch09_orgnz_files/shell_util.py
@@ -0,0 +1,9 @@
+#shutil 可以理解为 shell utilities shell下的工具箱
+
+import shutil as su, os
+
+su.copy("./first.txt", "./A/B/C") # copy file to new destionation
+
+su.copy("./first.txt", "./A/B/renamed.txt") # copy file to new destination and renamed the file
+
+print(os.listdir("./"))
