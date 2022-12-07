51899397b73772be35de46304a6182e72784c692
Chao Jiang <co.jiaan@proton.me>
2022-12-07 21:40:35 +0800

    auto push 2022/12/07@21:40:35

--- python_automation/ch09_orgnz_files/compression/extract.py
+++ python_automation/ch09_orgnz_files/compression/extract.py
@@ -4,5 +4,5 @@ exampleZip = zf.ZipFile("./chap7.zip")
 
 print(exampleZip.namelist())
 
-exampleZip.extractall("./extracted_By_Python")
-exampleZip.extract("pc2.py", "partial_extraction_by_Python")
+exampleZip.extractall("./extracted_By_Python") # extract all files to a designated folder
+exampleZip.extract("pc2.py", "partial_extraction_by_Python") # extract some files to a designated folder
