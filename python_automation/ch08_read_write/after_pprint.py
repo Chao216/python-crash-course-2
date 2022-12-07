41d54795e389d409f25b469d0f8bcdab65409dca
Chao Jiang <co.jiaan@proton.me>
2022-12-07 19:43:52 +0800

    auto push 2022/12/07@19:43:52

---
+++ python_automation/ch08_read_write/after_pprint.py
@@ -0,0 +1,11 @@
+import myDogs
+
+print(myDogs.dogs)
+print(myDogs.dogs[0])
+print(myDogs.dogs[0]["name"])
+print(myDogs.dogs[0]["age"])
+print(myDogs.dogs[1])
+print(myDogs.dogs[1]["name"])
+print(myDogs.dogs[1]["age"])
+
+
