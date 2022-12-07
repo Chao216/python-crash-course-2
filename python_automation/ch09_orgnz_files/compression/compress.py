f69aafb227177b505767f06f15b80c03c8e93d55
Chao Jiang <co.jiaan@proton.me>
2022-12-07 21:32:24 +0800

    auto push 2022/12/07@21:32:24

--- python_automation/ch09_orgnz_files/compression/compress.py
+++ python_automation/ch09_orgnz_files/compression/compress.py
@@ -7,6 +7,8 @@ print(exampleZip.namelist())
 
 joinInfo = exampleZip.getinfo("join.py")
 
+print("join.py file size. \n")
 print(joinInfo.file_size)
+print("join.py compressed size \n")
 print(joinInfo.compress_size)
 
