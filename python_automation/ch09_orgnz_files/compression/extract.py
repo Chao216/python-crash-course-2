c2c41afafebdb430eb97c97a8a94ac0a34d621fb
Chao Jiang <co.jiaan@proton.me>
2022-12-07 21:41:21 +0800

    auto push 2022/12/07@21:41:21

--- python_automation/ch09_orgnz_files/compression/extract.py
+++ python_automation/ch09_orgnz_files/compression/extract.py
@@ -6,3 +6,5 @@ print(exampleZip.namelist())
 
 exampleZip.extractall("./extracted_By_Python") # extract all files to a designated folder
 exampleZip.extract("pc2.py", "partial_extraction_by_Python") # extract some files to a designated folder
+
+exampleZip.close()
