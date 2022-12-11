1ec261c6247314315dbcd622f5bda82aa50fa139
Chao Jiang <co.jiaan@proton.me>
2022-12-11 19:54:32 +0800

    auto push 2022/12/11@19:54:32

---
+++ cat_string.py
@@ -0,0 +1,16 @@
+a = 11
+b = True
+
+c = 11.2345
+
+print(a,b,c)
+
+print("I like coffee.", b)
+
+message = "What numbers? "
+print(message + str(a) + " and "+ str(c))
+
+print("you can only concatenate string with +")
+print("if they are different type data, use , instead")
+
+print("your son is now ", a, " years old.")
